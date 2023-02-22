7eede11c1ff4b4ebcccab687373d7c2b5f1eae84
IvanovM <IvanovM@fizpribor.ru>
2023-02-22 21:06:06 +0300

    11 - fixed intersection

--- t11/t11.go
+++ t11/t11.go
@@ -4,20 +4,41 @@ import (
 	"fmt"
 )
 
+// func union(arr_1, arr_2 []int) []int {
+// 	res := []int{}
+
+// 	m := make(map[int]bool)
+
+// 	for _, val := range arr_1 {
+// 		m[val] = true
+// 	}
+// 	for _, val := range arr_2 {
+// 		m[val] = true
+// 	}
+
+// 	for key := range m {
+// 		res = append(res, key)
+// 	}
+
+// 	return res
+// }
+
 func intersection(arr_1, arr_2 []int) []int {
 	res := []int{}
 
-	m := make(map[int]bool)
+	m := make(map[int]int)
 
 	for _, val := range arr_1 {
-		m[val] = true
+		m[val] += 1
 	}
 	for _, val := range arr_2 {
-		m[val] = true
+		m[val] += 1
 	}
 
-	for key := range m {
-		res = append(res, key)
+	for key, val := range m {
+		if val == 2 {
+			res = append(res, key)
+		}
 	}
 
 	return res
